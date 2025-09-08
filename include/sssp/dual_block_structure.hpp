e15cfaf16a768fff4210fe5424cf317c9f10af75
AhuviBloy <a0527178668@gmail.com>
2025-09-08 17:28:45 +0300

    implement D0, hash, dual_block_structure

--- include/sssp/dual_block_structure.hpp
+++ include/sssp/dual_block_structure.hpp
@@ -1,17 +1,48 @@
-// dual_block_structure.hpp
-enum class InsertResult { NoChange, InsertedNew, Decreased };
+#pragma once
+#include <list>
+#include <cstddef>
+#include <utility>
+#include <algorithm>
+#include <cassert>
 
-template<class Key>
+
+#include "src/lib/internal/ds_common.hpp" 
+
+template <class Key> class D0;
+template <class Key> class D1;
+template <class Key> class Hash;
+
+template <class Key>
 class DualBlockStructure {
 public:
-    //insert new node
-    InsertResult insert_or_decrease(const Key& k, double newVal);
+    using KV = ds::KV<Key>;
+    using Block = ds::Block<Key>;
+    using KVList = std::list<KV>;
+
+    DualBlockStructure(std::size_t M, std::size_t B, std::size_t expected_keys = 0);
+
+    std::size_t M() const noexcept { return M_; }
+    std::size_t B() const noexcept { return B_; }
+
+    void batch_prepend(KVList&& items);
+
+    KVList pull();
+
+    bool insert(const Key& key, double value);
+
+    D0<Key>& d0()       noexcept { return d0_; }
+    const D0<Key>& d0() const noexcept { return d0_; }
+    D1<Key>& d1()       noexcept { return d1_; }
+    const D1<Key>& d1() const noexcept { return d1_; }
+    const Hash<Key>& map() const noexcept { return hash_; }
 
 private:
-    // choose a block in d1 by search in the tree
-    typename D1<Key>::BlockIt choose_block_for_value_in_d1(double v);
+    std::size_t M_;
+    std::size_t B_;
 
     D0<Key>   d0_;
     D1<Key>   d1_;
     Hash<Key> hash_;
+
+    void erase_existing_(const ds::Handle<Key>& h);
 };
